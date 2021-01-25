f795de8800dd475e4d58b21d4a4d157f21081e49
Afdhal_FA <fuad.afdal.fa@gmail.com>
2021-01-25 22:38:45 +0700

    update make activity

--- baseProjectLibrary/src/main/java/com/afdhal_fa/baseprojectlibrary/base/BaseFragment.kt
+++ baseProjectLibrary/src/main/java/com/afdhal_fa/baseprojectlibrary/base/BaseFragment.kt
@@ -1,5 +1,6 @@
 package com.afdhal_fa.baseprojectlibrary.base
 
+import android.app.Activity
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
@@ -10,10 +11,12 @@ abstract class BaseFragment<viewmodel : ViewModel> : Fragment(), IBaseView {
     private val TAG = BaseFragment::class.java.simpleName
 
     protected lateinit var viewmodel: viewmodel
+    protected lateinit var activity: Activity
     protected abstract fun initViewModel(): viewmodel
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        this.activity = requireActivity()
         viewmodel = initViewModel()
     }
 
